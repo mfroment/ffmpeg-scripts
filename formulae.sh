13b5136cb244bad93f5b04b288f06dd38056c64c
Maxime Froment <mfroment99@gmail.com>
2025-03-30 21:58:53 +0900

    Add an example for vertical height crop + mirror

--- formulae.sh
+++ formulae.sh
@@ -7,3 +7,16 @@ ffmpeg -i input.mp4 -i mirror.mp4 -filter_complex \
  [1:v]scale=iw:ih[s];[s]pad=w=iw:h=ih*3:x=0:y=0[small]; \
  [small][base]hstack=inputs=2" \
 -map 0:a -c:a copy output.mp4
+
+# Create a video with a vertical crop, the original in the bottom half and the mirror in the top half
+# Here the crop is 1920x660 starting at (0,318)
+ffmpeg -i input.mp4  \
+-filter_complex \
+"[0:v]crop=w=1920:h=660:x=0:y=318[cropped]; \
+ [cropped]split=2[original][toflip]; \
+ [toflip]hflip[flipped]; \
+ [flipped][original]vstack=inputs=2[output]" \
+-map "[output]" \
+-map 0:a \
+-c:a copy \
+output.mp4
